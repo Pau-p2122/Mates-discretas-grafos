d79348a3b50abc4e3ff37f4133a6e9b462ffb138
Pau-p2122 <ppasten2026@alu.uct.cl>
2026-06-18 19:56:58 -0400

    Import
    puse los import que deberiamos necesitar

---
+++ import networkx as nx.py
@@ -0,0 +1,6 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import customtkinter as tk
+from tkinter import messagebox 
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import csv
